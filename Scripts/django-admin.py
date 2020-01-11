0a232a471911340d5863ceab13de9dd37a728175
chutithep88 <chutithep.job@gmail.com>
2020-01-11 21:33:19 +0700

    backup testsearch

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\job\desktop\p4t1\allproject\django\djangosearch\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
